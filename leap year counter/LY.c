52dc0021dfe40c07bac8226d8254d730411d0db7
MD. Rafiul Islam <rafiulislam316@gmail.com>
2025-08-13 17:08:32 +0600

    Update LY.c

--- leap year counter/LY.c
+++ leap year counter/LY.c
@@ -4,12 +4,13 @@ int main(){
     int year;
     printf("Enter a valid year:");
     scanf("%d",&year);
-    if(year%100==0 || year%400==0 || year%4==0){
+    if(year%4==0 && year%100!=0){
         printf("It's a leap year !!");
     }
-    else{
-        printf("it's not a leap year.");
+    else if(year%400==0){
+        printf("it's a leap year.");
     }
+    else{printf("It's not a leap year !!");}
 
 
 return 0;
