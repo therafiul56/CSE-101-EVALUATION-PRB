78a210f274fd47e7be6efc7449aa57911fe9b0c3
MD. Rafiul Islam <rafiulislam316@gmail.com>
2025-08-15 14:01:33 +0600

    Create C-f.c
    3. Write a C program to input temperature in Centigrade and convert to Fahrenheit. How to convert temperature from degree centigrade to degree Fahrenheit in C programming. Logic to convert temperature from Celsius to Fahrenheit in C.

---
+++ 2.class problem/convert Centigrade to Fahrenheit/C-f.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include<math.h>
+/*_*/
+int main(){
+    float C,F;
+    printf("Enter the value in celcius:");
+    scanf("%f",&C);
+    F=(9*C/5)+32;
+    printf("The temperature in Fahrenheitis : %.2f °F",F);
+
+return 0;
+}
