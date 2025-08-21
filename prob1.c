5b5339f12b1a274322a87dac9b1e3ab2f761e159
MD. Rafiul Islam <rafiulislam316@gmail.com>
2025-08-21 23:57:29 +0600

    Update prob1.c

--- prob1.c
+++ prob1.c
@@ -6,7 +6,7 @@ int main() {
     scanf("%d",&num);
     if (num>0){
         printf("%d is a positive number",num);
-    }else if(num=0) {  
+    }else if(num==0) {  
         printf("%d is zero",num);
     }else {
         printf("%d is negative",num);
